89316926cbc01f65f05b0887d56466799f8796ff
root <caio.bsampaio@gmail.com>
2022-03-05 22:41:08 +0100

    Raspberry Serial Communication Update

--- Raspberry/SerialGyroscopeControlArduino/SerialGyroscopeControlArduino.py
+++ Raspberry/SerialGyroscopeControlArduino/SerialGyroscopeControlArduino.py
@@ -1,8 +1,21 @@
 import serial
-from time import sleep
+import time
 
-ser = serial.Serial('/dev/ttyS0', 115200)
-while True:
-    message = ser.read(8)
-    print(message);
-    sleep(1)
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
+    
+    while True:
+        try :
+            ser.flushInput()
+            ser.flushOutput()
+            time.sleep(0.05)
+            ser.write((str("#" + str(round(time.time())) + "%")).encode('utf-8'))            
+            while 1 :
+                if ser.inWaiting():
+                    line = ser.readline().decode('utf-8').rstrip()
+                    print(line)
+                    
+                    break
+            time.sleep(0.07)
+        except :
+            ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
